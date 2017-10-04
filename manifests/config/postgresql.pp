07d9b3c7aeae1757fe2e85e585701c88f1cd1fb2
Samuli Seppänen <samuli@openvpn.net>
2017-10-04 14:35:18 +0000

    Manage a fixed version of grant-all.postgres.sql
    The version in Git for Patchwork tag v2.0.0 is for a more recent patchwork
    version and does not work. It also assumed a RedHat derivative. With this commit
    a fixed version of the SQL file is copied over to the target server, but not yet
    loaded automatically.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/config/postgresql.pp
+++ manifests/config/postgresql.pp
@@ -31,4 +31,14 @@ class patchwork::config::postgresql
         environment => "NEWPGPASSWD=${password_hash}",
         unless      => "SELECT 1 FROM pg_roles WHERE rolname = 'patchwork'",
     }
+
+    # Copy over a fixed grant-all.postgres.sql script
+    file { '/opt/patchwork/lib/sql/grant-all.postgres.puppet.sql':
+        ensure => 'present',
+        content => template('patchwork/grant-all.postgres.sql.erb'),
+        owner   => $::os::params::adminuser,
+        group   => $::os::params::admingroup,
+        mode    => '0644',
+        require => Class['::patchwork::install'],
+    }
 }

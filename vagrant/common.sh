4f54e5f7ac740a17ff87dcdc3d1f8183913a784e
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2022-06-07 11:54:13 +0300

    Vagrant: remove last traces of the obsolete provisioning code
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- vagrant/common.sh
+++ vagrant/common.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin
 
 wget https://raw.githubusercontent.com/Puppet-Finland/scripts/master/bootstrap/linux/install-puppet.sh -q -O install-puppet.sh
-/bin/sh /vagrant/vagrant/install-puppet.sh -n patchwork -p 7
+/bin/sh install-puppet.sh -n patchwork -p 7
 
 wget https://raw.githubusercontent.com/Puppet-Finland/scripts/master/bootstrap/linux/install-puppet-modules.sh -q -O install-puppet-modules.sh
 /bin/sh install-puppet-modules.sh -n patchwork
